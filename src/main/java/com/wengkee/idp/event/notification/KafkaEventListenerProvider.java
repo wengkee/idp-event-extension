c3f873b4d853d3c0efcccbc99da6863af406a5ed
Weng Kee Teh <wengkee.teh@gmail.com>
2022-04-03 02:11:51 +0800

    added event listener and kafka producer

--- src/main/java/com/wengkee/idp/event/notification/KafkaEventListenerProvider.java
+++ src/main/java/com/wengkee/idp/event/notification/KafkaEventListenerProvider.java
@@ -1,114 +1,80 @@
 package com.wengkee.idp.event.notification;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
 
-import java.util.Map;
+import org.jboss.logging.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 public class KafkaEventListenerProvider implements EventListenerProvider{
 
+    private static final Logger LOGGER = Logger.getLogger(KafkaEventListenerProvider.class);
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    private String kafkaTopic;
+    private List<EventType> keycloakEventTypes;
+    private Producer<String, String> kafkaProducer;
+
+    public KafkaEventListenerProvider(String kafkaBootstrapServers, String kafkaClientID, String kafkaTopic, String[] keycloakEventTypes){
+
+        this.kafkaTopic = kafkaTopic;
+        this.keycloakEventTypes = new ArrayList<>();
+
+        for ( String keycloakEvent : keycloakEventTypes ){
+            try {
+                EventType eventType = EventType.valueOf(keycloakEvent.toUpperCase());
+                this.keycloakEventTypes.add(eventType);
+            } catch (IllegalArgumentException e){
+                LOGGER.debug("Event: [" + keycloakEvent + "] doesn't exists.");
+            }
+        }
+
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServers);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, kafkaClientID);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        kafkaProducer = new KafkaProducer<>(props);
+    }
+
     @Override
     public void onEvent(Event event) {
-        System.out.println("Event Occurred: " + toString(event));
+        if (keycloakEventTypes.contains(event.getType())) {
+            dispatchEvent(event);
+        }
     }
 
     @Override
     public void onEvent(AdminEvent adminEvent, boolean b) {
-        System.out.println("Admin Event Occurred:" + toString(adminEvent));
-    }
-
-    private String toString(Event event) {
-
-        StringBuilder sb = new StringBuilder();
-
-        sb.append("type=");
-        sb.append(event.getType());
-        sb.append(", realmId=");
-        sb.append(event.getRealmId());
-        sb.append(", clientId=");
-        sb.append(event.getClientId());
-        sb.append(", userId=");
-        sb.append(event.getUserId());
-        sb.append(", ipAddress=");
-        sb.append(event.getIpAddress());
-
-        if (event.getError() != null) {
-
-            sb.append(", error=");
-
-            sb.append(event.getError());
-
-        }
-
-
-        if (event.getDetails() != null) {
-
-            for (Map.Entry<String, String> e : event.getDetails().entrySet()) {
-
-                sb.append(", ");
-                sb.append(e.getKey());
-                if (e.getValue() == null || e.getValue().indexOf(' ') == -1) {
-                    sb.append("=");
-                    sb.append(e.getValue());
-
-                } else {
-                    sb.append("='");
-                    sb.append(e.getValue());
-                    sb.append("'");
-                }
-            }
-
-        }
-
-        return sb.toString();
-    }
-
-    private String toString(AdminEvent adminEvent) {
-
-        StringBuilder sb = new StringBuilder();
-
-
-        sb.append("operationType=");
-
-        sb.append(adminEvent.getOperationType());
-
-        sb.append(", realmId=");
-
-        sb.append(adminEvent.getAuthDetails().getRealmId());
-
-        sb.append(", clientId=");
-
-        sb.append(adminEvent.getAuthDetails().getClientId());
-
-        sb.append(", userId=");
-
-        sb.append(adminEvent.getAuthDetails().getUserId());
-
-        sb.append(", ipAddress=");
-
-        sb.append(adminEvent.getAuthDetails().getIpAddress());
-
-        sb.append(", resourcePath=");
-
-        sb.append(adminEvent.getResourcePath());
-
-
-        if (adminEvent.getError() != null) {
-
-            sb.append(", error=");
-
-            sb.append(adminEvent.getError());
-
-        }
-
-
-        return sb.toString();
-
+//        if (adminEvent != null) {
+//            dispatchEvent(adminEvent);
+//        }
     }
 
     @Override
-    public void close() {
+    public void close() {}
 
+    private void dispatchEvent(Object event)  {
+        try {
+
+            String json = MAPPER.writeValueAsString(event);
+            kafkaProducer.send(new ProducerRecord<>(kafkaTopic, json));
+
+        } catch (JsonProcessingException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
     }
 }
--- src/main/java/com/wengkee/idp/event/notification/KafkaEventListenerProviderFactory.java
+++ src/main/java/com/wengkee/idp/event/notification/KafkaEventListenerProviderFactory.java
@@ -1,35 +1,65 @@
 package com.wengkee.idp.event.notification;
 
+import org.jboss.logging.Logger;
 import org.keycloak.Config;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventListenerProviderFactory;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 
+import java.util.Arrays;
+
 public class KafkaEventListenerProviderFactory implements EventListenerProviderFactory {
+
+    private static final Logger LOGGER = Logger.getLogger(KafkaEventListenerProviderFactory.class);
+    private static final String ID = "kafka_event_listener";
+
+    private KafkaEventListenerProvider kafkaEventListenerProvider;
+
+    private String kafkaBootstrapServers;
+    private String kafkaClientID;
+    private String kafkaTopic;
+    private String[] keycloakEventTypes;
 
     @Override
     public EventListenerProvider create(KeycloakSession keycloakSession) {
-        return new KafkaEventListenerProvider();
+        if (kafkaEventListenerProvider == null){
+            kafkaEventListenerProvider = new KafkaEventListenerProvider(kafkaBootstrapServers, kafkaClientID, kafkaTopic, keycloakEventTypes);
+        }
+        return kafkaEventListenerProvider;
     }
 
     @Override
     public void init(Config.Scope scope) {
 
+        kafkaTopic = scope.get("KAFKA_TOPIC", System.getenv("KAFKA_TOPIC"));
+        kafkaClientID = scope.get("KAFKA_CLIENT_ID", System.getenv("KAFKA_CLIENT_ID"));
+        kafkaBootstrapServers = scope.get("KAFKA_BOOTSTRAP_SERVERS", System.getenv("KAFKA_BOOTSTRAP_SERVERS"));
+
+        String eventTypesString = scope.get("KEYCLOAK_EVENT_TYPES", System.getenv("KEYCLOAK_EVENT_TYPES"));
+        if (eventTypesString != null){
+            keycloakEventTypes = eventTypesString.split(",");
+        } else {
+            keycloakEventTypes = new String[1];
+            keycloakEventTypes[0] = "LOGIN";
+        }
+
+        LOGGER.info("Initializing Kafka Event Listener..");
+        LOGGER.info("KAFKA_TOPIC: [" + kafkaTopic + "]");
+        LOGGER.info("KAFKA_CLIENT_ID: [" + kafkaClientID + "]");
+        LOGGER.info("KAFKA_BOOTSTRAP_SERVERS: [" + kafkaBootstrapServers + "]");
+        LOGGER.info("KEYCLOAK_EVENT_TYPES: [" + Arrays.toString(keycloakEventTypes) + "]");
+
     }
 
     @Override
-    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
-
-    }
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {}
 
     @Override
-    public void close() {
-
-    }
+    public void close() {}
 
     @Override
     public String getId() {
-        return "kafka_event_listener";
+        return ID;
     }
 }
